f4ea4a4fa6d6f1009eeac429f809f2882fd9f8c5
Edward Verity <eddieverity@gmail.com>
2017-02-26 14:02:12 -0600

    added more

---
+++ WebFundamentals/Javascript/dany_object.js
@@ -0,0 +1,10 @@
+var Dany = {
+  first_name: "Daenerys",
+  last_name: "Targaryen",
+  age: 22,
+  alliegence: "House Targaryen",
+  ancestry: "Valyrian",
+  titles: ["Queen of the Andals" , "Lady Regent of the Seven Kingdoms" , "Protector of the Realm"]
+}
+console.log(Dany.alliegence);
+console.log(Dany.titles);
