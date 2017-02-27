ec22b0aa426ccb4c288009a2ff5f1f0a8da3ff3a
Edward Verity <eddieverity@gmail.com>
2017-02-27 13:28:06 -0600

    adding more

---
+++ WebFundamentals/Javascript/DOM/JSpage.js
@@ -0,0 +1,4 @@
+console.log(document.body);
+var body = document.body;
+// this is more fun if it's a random page with stuff already on it (like the learning platform for example).
+body. innerHTML = "Hello World";
