a43da2075412f60f14490f9d2355d7480afba126
Edward Verity <eddieverity@gmail.com>
2017-03-03 08:31:50 -0600

    adjusted some api files, got.js had issue with chrome browser

--- WebFundamentals/api_ajax/got/got.js
+++ WebFundamentals/api_ajax/got/got.js
@@ -1,11 +1,28 @@
 $(document).ready (function() {
+
+
+
+
+
   $(document).on("click","img", function(){
     var imgid = $(this).attr("id");
     $.get("http://www.anapioficeandfire.com/api/houses/"+imgid, function(res) {
+
+      
+
+
+
+
+
       var html_str = "<h3>House Details</h3>";
-      html_str += "<p>Name:"+ res.name +"</p>";
-      html_str += "<p>Words:"+ res.words +"</p>";
-      html_str += "<p>Titles:" + res.titles +"</p>"      
+      html_str = html_str + "<p>Name:"+ res.name +"</p>";
+      html_str = html_str + "<p>Words:"+ res.words +"</p>";
+      html_str = html_str + "<p>Titles:"
+      for (var i = 0;i<res.titles.length;i++) {
+        html_str = html_str + res.titles[i] + " , "
+      }
+      html_str = html_str + "</p>"
+      console.log(html_str)      
       $("#stats").html(html_str);
     }, "json");
   });
