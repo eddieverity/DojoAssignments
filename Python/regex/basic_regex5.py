81a1bab993bb0ae1fc2026d96cb0d2872cae3139
Edward Verity <eddieverity@gmail.com>
2017-03-06 16:35:08 -0600

    had a bunch of commit errors due to having virutal environment set in here, now cleaned up

---
+++ Python/regex/basic_regex5.py
@@ -0,0 +1,10 @@
+import re
+def get_matching_words():
+  words = ["aimlessness", "assassin", "baby", "beekeeper", "belladonna", "cannonball", "crybaby", "denver", "embraceable", "facetious", "flashbulb", "gaslight", "hobgoblin", "iconoclast", "issue", "kebab", "kilo", "laundered", "mattress", "millennia", "natural", "obsessive", "paranoia", "queen", "rabble", "reabsorb", "sacrilegious", "schoolroom", "tabby", "tabloid", "unbearable", "union", "videotape"]
+  matches = []
+  for word in words:
+    if re.search(r"b*b",word):
+      matches.append(word)
+  #return matches
+  print matches
+get_matching_words()
